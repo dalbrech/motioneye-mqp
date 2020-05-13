f99c3d01b9942cb35d36f471e9506b7933f98eba
dalbrech <46980704+dalbrech@users.noreply.github.com>
2020-05-13 06:08:07 -0400

    Update setup_motioneye.sh

--- setup_motioneye.sh
+++ setup_motioneye.sh
@@ -1,9 +1,9 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install ffmpeg libmariadb3 libpq5 libmicrohttpd12
+sudo apt-get install -y ffmpeg libmariadb3 libpq5 libmicrohttpd12
 sudo wget https://github.com/Motion-Project/motion/releases/download/release-4.2.2/pi_buster_motion_4.2.2-1_armhf.deb
 sudo dpkg -i pi_buster_motion_4.2.2-1_armhf.deb
-sudo apt-get install python-pip python-dev libssl-dev libcurl4-openssl-dev libjpeg-dev libz-dev
+sudo apt-get install -y python-pip python-dev libssl-dev libcurl4-openssl-dev libjpeg-dev libz-dev
 sudo pip install motioneye
 sudo mkdir -p /etc/motioneye
 sudo cp /usr/local/share/motioneye/extra/motioneye.conf.sample  /etc/motioneye/motioneye.conf
