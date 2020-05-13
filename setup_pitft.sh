e2badbafb4d5c7374a954c62b0b6068b69fb60cd
dalbrech <46980704+dalbrech@users.noreply.github.com>
2020-05-13 08:04:04 -0400

    Update setup_pitft.sh

--- setup_pitft.sh
+++ setup_pitft.sh
@@ -2,6 +2,6 @@ sudo pip3 install RPI.GPIO
 sudo pip3 install adafruit-blinka
 sudo pip3 install adafruit-circuitpython-rgb-display
 sudo pip3 install --upgrade --force-reinstall spidev
-sudo apt-get install ttf-dejavu
-sudo apt-get install python3-pil
-sudo apt-get install python3-numpy
+sudo apt-get install -y ttf-dejavu
+sudo apt-get install -y python3-pil
+sudo apt-get install -y python3-numpy
