7c2a0ea231086a8d060ebc6e426355b8fa416d90
dalbrech <46980704+dalbrech@users.noreply.github.com>
2020-05-13 07:47:31 -0400

    Create setup_pitft.sh

---
+++ setup_pitft.sh
@@ -0,0 +1,7 @@
+sudo pip3 install RPI.GPIO
+sudo pip3 install adafruit-blinka
+sudo pip3 install adafruit-circuitpython-rgb-display
+sudo pip3 install --upgrade --force-reinstall spidev
+sudo apt-get install ttf-dejavu
+sudo apt-get install python3-pil
+sudo apt-get install python3-numpy
